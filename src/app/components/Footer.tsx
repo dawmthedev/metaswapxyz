7e61c33fccd148a492e8aa7c14e0bec322f4f0ce
dawmthedev <dominiqmartinez13@gmail.com>
2025-01-03 20:08:25 -0800

    Cleaned up lava lamp effect

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,26 +1,39 @@
 "use client";
 
 export default function Footer() {
-  //working
   return (
-    <footer className="absolute bottom-0 left-0 w-full px-12 py-8 z-10 flex justify-center mb-2">
-      <div className="flex flex-col items-start max-w-[70%]">
-        <h3 className="text-5xl font-extrabold leading-none">Metaswap</h3>
-        <div className="flex gap-8 mt-5">
-          <a href="#" className="underline text-m font-medium hover:text-white">
+    <footer className="absolute bottom-0 left-0 w-full px-4 md:px-12 py-4 md:py-8 z-10 flex justify-center mb-2">
+      <div className="flex flex-col items-start w-full md:max-w-[70%]">
+        <h3 className="text-3xl md:text-5xl font-extrabold leading-none">
+          Metaswap
+        </h3>
+        <div className="flex flex-wrap gap-4 md:gap-8 mt-3 md:mt-5">
+          <a
+            href="#"
+            className="underline text-sm md:text-m font-medium hover:text-white"
+          >
             MSC
           </a>
-          <a href="#" className="underline text-m font-medium hover:text-white">
+          <a
+            href="#"
+            className="underline text-sm md:text-m font-medium hover:text-white"
+          >
             Bit Map
           </a>
-          <a href="#" className="underline text-m font-medium hover:text-white">
+          <a
+            href="#"
+            className="underline text-sm md:text-m font-medium hover:text-white"
+          >
             Unhash Labs
           </a>
-          <a href="#" className="underline text-m font-medium hover:text-white">
+          <a
+            href="#"
+            className="underline text-sm md:text-m font-medium hover:text-white"
+          >
             Connect
           </a>
         </div>
-        <p className="text-[0.7rem] font-normal text-black mt-5">
+        <p className="text-[0.6rem] md:text-[0.7rem] font-normal text-black mt-3 md:mt-5">
           © 2025 Metaswap, LLC. Metaswap and the Metaswap Logo are trademarks of
           Metaswap, LLC.{" "}
           <a href="#" className="hover:underline font-bold">
