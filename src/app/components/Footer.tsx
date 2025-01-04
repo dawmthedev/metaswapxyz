db06e586cb8c90c4a9d75c33500374ccb35ef5e3
dawmthedev <dominiqmartinez13@gmail.com>
2025-01-03 18:33:04 -0800

    Add

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export default function Footer() {
+  //working
   return (
     <footer className="absolute bottom-0 left-0 w-full px-12 py-8 z-10 flex justify-center items-center mb-10">
       {/* Left Section */}
